3306fb652cefad5a28e3fbb55081c1fc005ab892
PAULGOYES <goyes.yesid@gmail.com>
2019-12-05 22:47:28 -0500

    Add files via upload

---
+++ CAPITULO_02/Script_2_4.sh
@@ -0,0 +1,30 @@
+#####################################################################
+#                                                                   #
+#  SEISMIC UN*X PARA LA ENSEÑANZA DEL MÉTODO DE REFLEXIÓN SÍSMICA   #
+#           Capítulo 2: Empezando con Unix y Seismic Unix           #
+#         Script 2.4: Graficar archivos migrados de simple.su       #
+#  En este script se grafican los archivos de migración solt, phase #
+#  shift y por diferencias finitas                                  #
+#                                                                   #
+#####################################################################
+
+#! bin/bash
+set -x
+
+file=simple.su
+stolt=stolt.$file
+ps=ps.$file
+fd=fd.$file
+
+rm -f $solt $ps $fd
+
+# MIGRACIÓN STOLT
+suxwigb < $stolt xcur=3 title="Migracion Stolt" label1="time" label2="trace" &
+
+# MIGRACIÓN PHASE-SHIFT
+suxwigb < $ps title="Migracion Phase shift" label1="time" label2="trace" xcur=3 &
+
+# MIGRACIÓN POR DIFERENCIAS FINITAS
+suxwigb < $fd title="Migracion por diferencias finitas" label1="depth" label2="trace" xcur=3 &
+
+exit
